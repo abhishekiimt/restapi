9b1432df3b20d0ccd71ea11489fd569978f4dece
Abhishek Pandey <abhishekbca15.iimtgn@gmail.com>
2021-08-04 14:56:44 +0530

    update commnet in router.js

--- route/router.js
+++ route/router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const DataBaseName = require('../model/dataBase')
-
+// change the database name coording to you
 
 //CRUD operation
 
